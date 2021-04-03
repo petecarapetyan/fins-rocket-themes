2e09503acc5abaf5c70c7cda920c45f4e456bdea
petecarapetyan <pete@datafundamentals.com>
2021-04-03 13:00:03 -0500

    WIP

--- try.sh
+++ try.sh
@@ -1,24 +1,43 @@
 #!/bin/bash
-# FOR EXPERIENCED SCRIPT USERS ONLY, ESPECIALLY `rm -rf` LINES
+# This script is not maintained for public use, kinda lame hacking
+# FOR EXPERIENCED SCRIPT USERS ONLY, use only after and accepting as your own
 
-# aborts if not 2 args, unless previously provided, which may be confusing
-if [ $# -lt 2 ]; then
-  echo 1>&2 "$0: Two arguments are expected - 1. project dir - 2. rocket theme. Example: `./try.sh myproject not-five38`"
+if [ $# -lt 4 ]; then
+  echo 1>&2 "$0: Four args `./try.sh myproject not-five38 [seed,keep], [start,not]`"
   exit 2
-elif [ $# -gt 2 ]; then
-  echo 1>&2 "$0: Too many arguments. Two arguments are expected - 1. project dir - 2. rocket theme"
+elif [ $# -gt 4 ]; then
+  echo 1>&2 "$0: Too many arguments. Only four arguments are expected - folder, theme, [seed,keep], [start,not]"
   exit 2
 fi
-
-# Sometimes uncommenting this next line can be helpful, if you want to try different themes without re-fetching node_modules
-rm -rf ../$1/docs # DANGER, WILL ROBINSON! But it can be helpful, if you're confident and want to try several themes in a row
-
 mkdir ../$1 # makes a sibling rocket project directory from your first arg
+mv ../$1/docs/ ../$1/docx
+mkdir -p ../$1/docs/_assets/_static
+if [ $3 = "seed" ]; then
+  cp -R ./seed-markdown/docs/* ../$1/docs/ # copies some Lorem Ipsum stuff into your project so you'll have data
+  rm -rf ../$1/docx
+elif [ $3 = "keep" ]; then
+  cp -R ../$1/docx/_assets/_static/* ../$1/docs/_assets/_static/
+  rm -rf ../$1/docx/_*
+  mv ../$1/docx/* ../$1/docs/
+  rm -rf ../$1/docx
+elif [ $3 = "empty" ]; then
+  rm -rf ../$1/docx
+else
+  echo "Third argument has to be seed, keep, or empty, NOT: $3"
+  exit 2
+fi
 cp -Rf ./seed-base-project/* ../$1 # copies the 3 of the 5 basic starter files into your new rocket project
 cp ./seed-base-project/.eleventyignore ../$1 
 cp ./seed-base-project/.gitignore ../$1
 cp -Rf ./$2/src/docs ../$1 # copies the theme named by your second argument, into your new project
-cp -R ./seed-markdown/docs/* ../$1/docs/ # copies some Lorem Ipsum stuff into your project so you'll have data
 cd ../$1 # changes directories to your new project
 npm i
-npm start
+if [ $4 = "start" ]; then
+  npm start
+elif [ $4 = "not" ]; then
+  echo "$1 was set up with $2 but not started"
+else
+  echo "Fourth argument has to be 'start', or 'not'"
+  exit 2
+fi
+  
