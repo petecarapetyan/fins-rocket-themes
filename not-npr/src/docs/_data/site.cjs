0907ae5e4b043006e9e20c6115bb518bc152bc29
petecarapetyan <pete@datafundamentals.com>
2021-04-07 19:45:12 -0500

    WIP

--- not-npr/src/docs/_data/site.cjs
+++ not-npr/src/docs/_data/site.cjs
@@ -7,7 +7,7 @@ module.exports = function () {
     socialLinks: [
       {
         name: 'GitHub',
-        url: 'https://github.com/petecarapetyan/rocket-themes/tree/main/not-npr/src/docs',
+        url: 'https://github.com/petecarapetyan/fins-rocket-themes/tree/main/not-npr/src/docs',
       },
     ],
     gitSiteUrl: 'https://github.com/petecarapetyan/appwriter',
